b4c0e845dce1c8978f8a73c366f31f2afe0b3399
nicodwi_k <nicodwika@gmail.com>
2020-10-14 23:20:57 +0700

    add api delete mentor

--- routes/mentors.js
+++ routes/mentors.js
@@ -5,6 +5,8 @@ const mentorsHandler = require('./handler/mentors');
 router.get('/', mentorsHandler.getAll)
 router.get('/:id', mentorsHandler.get)
 router.post('/', mentorsHandler.create)
+router.put('/:id', mentorsHandler.update)
+router.delete('/:id', mentorsHandler.destroy)
 // router.delete('/:id', mediaHandler.destroy)
 
 module.exports = router;
